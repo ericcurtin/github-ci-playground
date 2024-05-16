057a6693b1a69e1fb0414185dddfc58d354e983d
Eric Curtin <ecurtin@redhat.com>
2024-05-16 11:11:14 +0100

    verbose

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 
 DN="sample-images"
 git clone --depth=1 --recursive https://gitlab.com/CentOS/automotive/$DN.git
-$DN/automotive-image-builder/automotive-image-builder --container build \
-  osbuild-manifests/images/minimal.mpp.yml --export qcow2 --target qemu \
-  --verbose vm.qcow2
+$DN/automotive-image-builder/automotive-image-builder --verbose --container \
+  build osbuild-manifests/images/minimal.mpp.yml --export qcow2 --target qemu \
+  vm.qcow2
 
