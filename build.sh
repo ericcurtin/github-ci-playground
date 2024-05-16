3ead6f1f174c280f5edaf6e9a4a7f5718c6adbe8
Eric Curtin <ecurtin@redhat.com>
2024-05-16 11:04:46 +0100

    DN

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 
 set -ex
 
-FN_NO_EXT="sample-images-main"
-FN="$FN_NO_EXT.tar.gz"
-curl -OL https://gitlab.com/CentOS/automotive/sample-images/-/archive/main/$FN
-tar xvf $FN
-ls -ltr $FN_NO_EXT
-$FN_NO_EXT/automotive-image-builder/automotive-image-builder \
-  --container build images/minimal.mpp.yml --export qcow2 --target qemu vm.qcow2
+DN="sample-images"
+git clone --depth=1 --recursive https://gitlab.com/CentOS/automotive/$DN.git
+$DN/automotive-image-builder/automotive-image-builder --container build \
+  osbuild-manifests/images/minimal.mpp.yml --export qcow2 --target qemu vm.qcow2
 
